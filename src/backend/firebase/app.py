d42f110d123723e41bb3d5d111ca2c40b0382cc5
Michelle Lee <michelles.lee3153@gmail.com>
2025-01-25 15:53:01 -0800

    initial login code

--- src/backend/firebase/app.py
+++ src/backend/firebase/app.py
@@ -11,6 +11,26 @@ def hello():
     print("hello!")
     return "We received" + str(request.args.get("var"))
 
+
+@app.route("/login")
+def login():
+    patient_name = request.args.get("patient_name")
+    password_attempt = request.args.get("password")
+    login_data = db.collection(patient_name).document("login").get()
+
+    if login_data.exists:
+        password = login_data.to_dict().keys()
+        if password == password_attempt:
+            return jsonify({"attempt": "Log in successful!"})
+        else:
+            return jsonify({"attempt": "Wrong password."})
+    else:
+        db.collection(patient_name).document("login").set(password_attempt)
+
+
+"""
+Gets user name, email, birthday and adds it to Firebase.
+"""
 @app.route("/addprof")
 def create_profile():
     patient_name = request.args.get("patient_name")
@@ -19,10 +39,13 @@ def create_profile():
     profile = {"email": email, "birthday": birthday}
     db.collection(patient_name).document("profile").set(profile)
     return jsonify({"patient_name": patient_name,
-                     "patient_email": email,
+                    "patient_email": email,
                     "birthday": birthday})
     # return f"Patient name is {patient_name}. Email is {email}. Birthday is {birthday}."
 
+"""
+Gets user name, doctor's email, doctor's name, doctor's phone and adds it to Firebase.
+"""
 @app.route("/adddoc")
 def add_doctor_info():
     patient_name = request.args.get("patient_name")
@@ -36,16 +59,22 @@ def add_doctor_info():
                     "doctor_phone": phone})
 
 
+"""
+Gets user name, medicine name, how often medicine is taken, and dosage of medicine
+and adds it to Firebase.
+"""
 @app.route("/addmed")
 def add_medication():
     patient_name = request.args.get("patient_name")
     med_name  = request.args.get("med_name")
-    frequency = request.args.get("frequency")
+    #schedule is a string of a dictorary
+    #schedule = {days: [], time: []}
+    schedule = request.args.get("schedule")
     dosage = request.args.get("dosage")
-    medicine = {"name": med_name, "frequency": frequency, "dosage": dosage}
+    medicine = {"name": med_name, "frequency": schedule, "dosage": dosage}
     db.collection(patient_name).document("medicine").set(medicine)
     return jsonify({"med_name": med_name,
-                    "frequency": frequency,
+                    "schedule": schedule,
                     "dosage": dosage})
 
 
@@ -54,12 +83,12 @@ def add_medication():
 #     pass
 
 @app.route("/get")
-def get_medication():
+def get_medication():#
     patient_name = request.args.get("patient_name")
     coll = request.args.get("coll")
     field = request.args.get("field")
 
-    return db.collection(patient_name).document(coll).get(field)
+    return db.collection(patient_name).document(coll).get()
 
 if __name__ == "__main__":
 #     hello()
